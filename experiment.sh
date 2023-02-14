5ef161a9592c71b922571f07ea9228467f29f6b7
Ethan White <ethan@weecology.org>
2023-02-14 18:51:10 -0500

    Return experiment.sh to executing as sbatch
    Reverts unexplained change in behavior in
    a0c60f18c11b1accd3b6b401c48641632e12ef29

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,8 @@
+sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=EvSpecies   # Job name
 #SBATCH --mail-type=END               # Mail events
-#SBATCH --mail-user=benweinstein2010@gmail.com # Where to send mail
+#SBATCH --mail-user=ethanwhite@ufl.edu # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI r
 #SBATCH --cpus-per-task=30
@@ -10,8 +11,9 @@
 #SBATCH --output=/blue/ewhite/everglades/EvergladesSpeciesModel/logs/EvergladesSpeciesModel_%j.out   # Standard output and error log
 #SBATCH --error=/blue/ewhite/everglades/EvergladesSpeciesModel/logs/EvergladesSpeciesModel_%j.err
 #SBATCH --partition=gpu
-#SBATCH --gpus=a100:3
+#SBATCH --gpus=a100:1
 
 ulimit -c 0
 source activate ESM
 python everglades_species.py
+EOT
