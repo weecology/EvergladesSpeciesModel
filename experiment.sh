1440e202dd78caecffb55b3143915e7bad92e27c
Ethan White <ethan@weecology.org>
2023-05-02 13:00:23 -0400

    Run for longer on 1 GPU
    Single GPU training seems more stable

--- experiment.sh
+++ experiment.sh
@@ -5,8 +5,8 @@ sbatch <<EOT
 #SBATCH --mail-user=ethanwhite@ufl.edu # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI r
-#SBATCH --cpus-per-task=30
-#SBATCH --mem=140GB
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=60GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/blue/ewhite/everglades/EvergladesSpeciesModel/logs/EvergladesSpeciesModel_%j.out   # Standard output and error log
 #SBATCH --error=/blue/ewhite/everglades/EvergladesSpeciesModel/logs/EvergladesSpeciesModel_%j.err
